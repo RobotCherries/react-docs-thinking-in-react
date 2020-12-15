9ccab7e48ffff0a27041f3300a4c90e605e695ff
RobotCherries <zdip14@gmail.com>
2020-12-15 16:50:30 +0200

    1 file modified

--- index.tsx
+++ index.tsx
@@ -2,19 +2,15 @@ import React, { Component } from "react";
 import { render } from "react-dom";
 import { FilterableProductTable } from "./components";
 import "./style.css";
-import PRODUCTS from "./data/products.json";
 
 interface AppProps {}
 interface AppState {}
 
 export default class App extends Component<AppProps, AppState> {
-  products = PRODUCTS;
-
   render() {
-    console.log(this.products);
     return (
       <div>
-        // <FilterableProductTable products={this.products} />
+        <h1>Test</h1>
       </div>
     );
   }
